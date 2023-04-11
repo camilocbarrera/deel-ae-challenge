715f226c16e80a8cd90d10cbeec9550677a2abf1
camilocbarrera <cristian.correa.cs@gmail.com>
2023-04-11 04:28:00 -0500

    rpt layer

--- dbt_analytics/models/marts/warehouse/dim_dates.sql
+++ dbt_analytics/models/marts/warehouse/dim_dates.sql
@@ -1,6 +1,6 @@
 WITH dates AS (
      SELECT
-          DATEADD( DAY, ROW_NUMBER( ) OVER (ORDER BY NULL), '2019-12-31' )::DATE AS _date_day
+          DATEADD( DAY, ROW_NUMBER( ) OVER (ORDER BY NULL), '2017-12-31' )::DATE AS _date_day
         , DATE_TRUNC( 'day', _date_day )                                         AS day
         , DATE_TRUNC( 'month', _date_day )                                       AS month
         , DATE_TRUNC( 'week', _date_day )                                        AS week
@@ -8,7 +8,7 @@ WITH dates AS (
         , WEEK( _date_day )                                                      AS week_num
         , DAYNAME( _date_day )                                                   AS dayname
         , DAYOFWEEK( _date_day )                                                 AS day_of_week
-     FROM TABLE (GENERATOR( ROWCOUNT => ( 1461 ) ))
+     FROM TABLE (GENERATOR( ROWCOUNT => ( 3000 ) ))
 )
 SELECT *, COUNT( DISTINCT _date_day ) OVER (PARTITION BY month) days_of_month
 FROM dates
